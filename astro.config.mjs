44102e76d5fa83bd08c3171d9ff3d714a1b3b73a
Jose Vega <jocvegar@gmail.com>
2025-07-15 18:51:41 -0600

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fitoraquel.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(), vue()],
   vite: {
     plugins: [tailwindcss()],
   },
-  compress: true,
 });
