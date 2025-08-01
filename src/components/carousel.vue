bf2f885029dc9472f3c3016218cdfd25d59e6f27
Jose Vega <jocvegar@gmail.com>
2025-07-31 19:47:01 -0600

    carousel fix

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,14 +1,18 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import "vue3-carousel/carousel.css";
 import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
-// import car1 from "@/assets/car_1.jpg";
 import car2 from "@/assets/car_2.jpg";
 import car3 from "@/assets/car_3.jpg";
 import car4 from "@/assets/car_4.jpg";
 import car5 from "@/assets/car_5.jpg";
 
+const isClient = ref(false);
 const images = [
-  // { src: car1.src, alt: "Fito Diputado" },
+  { src: car2.src, alt: "Fito Diputado" },
+  { src: car3.src, alt: "Fito Diputado" },
+  { src: car4.src, alt: "Fito Diputado" },
+  { src: car5.src, alt: "Fito Diputado" },
   { src: car2.src, alt: "Fito Diputado" },
   { src: car3.src, alt: "Fito Diputado" },
   { src: car4.src, alt: "Fito Diputado" },
@@ -26,10 +30,14 @@ const config = {
     480: { itemsToShow: 1, mouseWheel: true },
   },
 };
+
+onMounted(() => {
+  isClient.value = true;
+});
 </script>
 
 <template>
-  <Carousel v-bind="config" class="py-8">
+  <Carousel v-if="isClient" v-bind="config" class="py-8">
     <Slide v-for="(image, index) in images" :key="index">
       <img :src="image.src" :alt="image.alt" />
     </Slide>
